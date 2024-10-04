c56af4c3fc71fa0abe3e6a757fd6e41cb8fd4dcd
Yusuf Aydın <yusayd3825@gmail.com>
2024-10-04 16:11:33 +0300

    created

---
+++ learn-language/learn_languageApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_languageApp.swift
+//  learn-language
+//
+//  Created by yusuf on 4.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct learn_languageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
