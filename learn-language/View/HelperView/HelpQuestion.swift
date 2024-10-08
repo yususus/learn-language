719271db23aadd1d31755057b3ca93516a43dc6c
Yusuf Aydın <yusayd3825@gmail.com>
2024-10-08 21:17:45 +0300

    bug fix json file

---
+++ learn-language/View/HelperView/HelpQuestion.swift
@@ -0,0 +1,20 @@
+//
+//  HelpQuestion.swift
+//  learn-language
+//
+//  Created by yusuf on 8.10.2024.
+//
+
+import SwiftUI
+
+struct HelpQuestion: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+        }
+    }
+}
+
+#Preview {
+    HelpQuestion()
+}
